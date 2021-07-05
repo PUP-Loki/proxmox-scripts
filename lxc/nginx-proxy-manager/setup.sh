1fdc7d9f6e8210badb73e7b834befc01fb6f5c80
PUP-Loki <75944209+PUP-Loki@users.noreply.github.com>
2021-07-05 16:22:48 +0100

    Update setup.sh

--- lxc/nginx-proxy-manager/setup.sh
+++ lxc/nginx-proxy-manager/setup.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 trap 'rm -rf $TMP' EXIT SIGINT SIGTERM
 
 TMP=/tmp/npm_install.sh
-URL=https://raw.githubusercontent.com/ej52/proxmox/main/lxc/nginx-proxy-manager/install.sh
+URL=https://raw.githubusercontent.com/PUP-Loki/proxmox-scripts/main/lxc/nginx-proxy-manager/install.sh
 
 rm -rf $TMP
 wget -q -O "$TMP" "$URL"
