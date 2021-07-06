d652fcb5889e8682580e2cda34a8bbd67ac9bd6d
PUP-Loki <75944209+PUP-Loki@users.noreply.github.com>
2021-07-06 11:28:28 +0100

    Update create.sh

--- lxc/nginx-proxy-manager/create.sh
+++ lxc/nginx-proxy-manager/create.sh
@@ -94,7 +94,7 @@ done
 _ctid=${_ctid:-`pvesh get /cluster/nextid`}
 _cpu_cores=${_cpu_cores:-1}
 _disk_size=${_disk_size:-2G}
-_host_name=${_host_name:-nginx-proxy-manager}
+_host_name=${_host_name:-nginx-proxy-manager.lokisys.icu}
 _bridge=${_bridge:-vmbr0}
 _memory=${_memory:-512}
 _swap=${_swap:-0}
